af31cf12fa133cc8be281a2ae9dcdb56a9f1d664
@dianavile <diana.vile@gmail.com>
2019-08-19 15:45:27 +0200

    Update script.vue

--- script.vue
+++ script.vue
@@ -1,58 +1,140 @@
+import {DatePicker} from 'uiv'
+import Vue from 'vue'
+Vue.directive('focus', { inserted(el) { el.focus() } })
+Vue.directive('selected', { inserted(el) { el.select() } })
 export default {
-    data() {
-      return {
+  components: {'my-table': require('@/components/vue-simple-table'), DatePicker},
+  data() {
+    return {
+      header: [
+        {
+          title: 'ID',
+          key: 'id',
+          class: 'text-center cursor-pointer'
+        },
+        {
+          title: 'Title',
+          key: 'title',
+          class: 'cursor-pointer'
+        },
+        {
+          title: 'Category',
+          key: 'category',
+          class: 'cursor-pointer'
+        },
+        {
+          title: 'Create At',
+          key: 'create',
+          class: 'text-center cursor-pointer'
+        },
+        {
+          title: 'Author',
+          key: 'author',
+          class: 'cursor-pointer'
+        }
+      ],
 
-        config: {
-        table_class: 'table table-hover',
-        head_class: 'bg-primary', 
-        body_class: 'well'
-      },
+      body: [{
+          class: 'text-center'
+        },
+        {
+          method: {
+            dblclick: 'edit',
+            slot: 'textarea'
+          }
+        },
+        {
+          method: {
+            dblclick: 'edit',
+            slot: 'select'
+          }
+        },
+        {
+          class: 'text-center',
+          method: {
+            dblclick: 'edit',
+            slot: 'calendar'
+          }
+        },
+        {
+          method: {
+            dblclick: 'edit',
+            slot: 'input'
+          }
+        }
+      ],
 
-        header: [
-          {
-            "title": "id",
-            "key": "id"
-          },
-          {
-            "title": "title",
-            "key": "title"
-          },
-          {
-            "title": "category name",
-            "key": "category"
-          },
-          {
-            "title": "create at ...",
-            "key": "create"
-          },
-          {
-            "title": "by",
-            "key": "author"
-          }
-        ],
-        data: [
-          {
-            "id": 1,
-            "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            "category": "Internet",
-            "create": "2017-02-03",
-            "author": "administrator"
-          },
-          {
-            "id": 2,
-            "title": "Sed hendrerit ex quis erat commodo condimentum.",
-            "category": "Cooking",
-            "create": "2017-09-15",
-            "author": "moderator"
-          },
-          {
-            "id": 3,
-            "title": "Maecenas egestas ligula et est accumsan suscipit.",
-            "category": "Default",
-            "create": "2016-11-21",
-            "author": "guest"
-          }
-        ]
+      data: [
+        {
+          id: 1,
+          title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+          category: 'Internet',
+          create: '2017-02-03',
+          author: 'administrator'
+        },
+        {
+          id: 2,
+          title: 'Sed hendrerit ex quis erat commodo condimentum.',
+          category: 'Cooking',
+          create: '2017-09-15',
+          author: 'moderator'
+        },
+        {
+          id: 3,
+          title: 'Maecenas egestas ligula et est accumsan suscipit.',
+          category: 'Default',
+          create: '2016-11-21',
+          author: 'guest'
+        }
+      ],
+
+      action: {
+        edit_value: '',
+        checkbox: [],
+        edit_row: {
+          id: '',
+          title: '',
+          category: '',
+          create: '',
+          author: ''
+        }
       }
+    }
+  },
+
+  methods: {
+    say(row) {
+      alert('Open console and looking for response object!')
+      console.log(row);
+    },
+    edit(value) {
+      this.action.edit_value = value;
+    },
+    submit(res) { 
+      if (res.row[res.k] != this.action.edit_value) {
+        let after_update = JSON.parse(JSON.stringify(res.row))
+        this.$refs.my_table.set_row_undo(after_update, res.c)
+        res.row[res.k] = this.action.edit_value
+      }
+      this.reset_edit()
+    },
+    reset_edit() {
+      this.$refs.my_table.close()
+    },
+    set_checkbox(item) {
+      this.action.checkbox = item
+    },
+    edit_row(row) {
+      for (let k in row) {
+        this.action.edit_row[k] = row[k]
+      }
+    },
+    submit_row(row) {
+      let after_update = JSON.parse(JSON.stringify(row))
+      for (let key in this.action.edit_row) {
+        row[key] = this.action.edit_row[key]
+      }
+      this.$refs.my_table.set_row_undo(after_update)
+      this.$refs.my_table.close(true)
     }
   }
